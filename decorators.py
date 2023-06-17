e11a1e0e413c36b986a31b553551a4608412fe79
EddyKubwimana <eddy.kubwimana@ashesi.edu.gh>
2023-06-17 23:30:42 +0000

    change in the decorators

--- decorators.py
+++ decorators.py
@@ -1,3 +1,8 @@
+
+def signal(f):
+     def wrapper():
+          f()
+     return wrapper
 class flight():
     def __init__(self, capacity):
         self.capacity = capacity
@@ -9,16 +14,26 @@ class flight():
                 return True
         print(name + "  was not added because there are no available seats")
 
-    
     def availableSeats(self):
         return self.capacity- len(self.passengers)
     
-
+@signal
+def sing():
+     print("I am tired")
 
 plane = flight(4)
 passengers = ["Eddy", "Diomede", "bartazare", "Eurica", "Samantha"]
 
 for person in passengers:
      plane.addPassenger(person)
+
+
+plane.availableSeats()
+
+sing()
+
+
+
+
 
     
